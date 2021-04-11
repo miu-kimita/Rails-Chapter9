85cd170472cc90cee9f0d2312de4d52a7d473a70
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-04-11 03:17:28 +0000

    [Finish]3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -29,6 +29,7 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
