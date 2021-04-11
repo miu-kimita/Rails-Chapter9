3eddc6565740fad7e4c97c26bb51074c2f84f544
miu-kimita <miu.6ki26.bigdaddy@gmail.com>
2021-04-11 03:15:56 +0000

    [Finish]3-7

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '77c44887ee01ab70d84c803ba5c8c9366b3b91ba706c602a7632d56945ff345b7c4008c64bfea6a72de0338f18c1ad6e4043e59a5f666b63b360ec361636fe8e'
